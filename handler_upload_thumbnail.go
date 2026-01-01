dfdaf45872d6d8c4b3f6ac9fcd7aebdf654f1e24
EthanColbert8 <emcolbert2@gmail.com>
2026-01-01 17:13:18 -0500

    handle thumbnail upload into server memory

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory int64 = 10 << 20
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +31,53 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Improper form body", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	newFile, newFileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Couldn't read file data", err)
+		return
+	}
+
+	fileType := newFileHeader.Header.Get("Content-Type")
+	if fileType == "" {
+		respondWithError(w, http.StatusBadRequest, "Improper file metadata", err)
+		return
+	}
+
+	fileData, err := io.ReadAll(newFile)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Failed to read file data", err)
+		return
+	}
+
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Couldn't find video data", err)
+		return
+	}
+
+	// validate that authenticated user owns the video
+	if videoMetadata.CreateVideoParams.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "User is not owner of video", nil)
+		return
+	}
+
+	newThumbnail := thumbnail{
+		mediaType: fileType,
+		data:      fileData,
+	}
+	videoThumbnails[videoID] = newThumbnail
+
+	newURL := fmt.Sprintf("http://localhost:%s/api/thumbnails/%s", cfg.port, videoID)
+	videoMetadata.ThumbnailURL = &newURL
+	cfg.db.UpdateVideo(videoMetadata)
+
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
